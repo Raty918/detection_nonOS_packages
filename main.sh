13f434b1fd6a8e5bfed60753a7e08c0ac13e30ae
Raty918 <raty918@gmail.com>
2017-06-27 13:45:37 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,21 +1,34 @@
 #!/bin/sh	
 
+echo "Operating system: $(cat /etc/os-release | grep -w ID | cut -d '=' -f 2)"
+
+#Command needed
+command -v ps >/dev/null 2>&1 || { echo >&2 "I require ps command but it's not installed.  Aborting."; exit 1; }
+command -v find >/dev/null 2>&1 || { echo >&2 "I require find but it's not installed.  Aborting."; exit 1; }
+command -v pmap >/dev/null 2>&1 || { echo >&2 "I require pmap but it's not installed.  Aborting."; exit 1; }
+
+
 #DEBIAN or UBUNTU
 if 
 cat /etc/os-release | grep -q 'ubuntu' ||
 cat /etc/os-release | grep -q 'debian'
 then
-	( exec "./detection.sh" )	
-fi
-	
-		#ALPINE
-		if cat /etc/os-release | grep -q 'alpine'
-		then
-			apk info -W $line >> tmpOS.list 2>> tmpnonOS.list
-		fi
+	( exec "./dpkg.sh" )	
+fi	
 		
-		#CENTOS
-		if cat /etc/os-release | grep -q 'centos'
-		then
-			rpm -qf $line >> tmpOS.list 2>> tmpnonOS.list
-		fi
+#ALPINE
+if 
+cat /etc/os-release | grep -q 'alpine'
+then
+	( exec "./apk.sh" )
+fi
+		
+#CENTOS or FEDORA or RHEL
+if 
+cat /etc/os-release | grep -q 'centos'
+cat /etc/os-release | grep -q 'fedora'
+cat /etc/os-release | grep -q 'rhel'
+then
+	( exec "./rpm.sh" )
+fi
+
